901a142b5a19013c8a3aefb59ecf792e854adc9e
Jean Oliveira <jc4281382@gmail.com>
2026-08-17 15:04:36 -0300

    Configura geracao automatica de sitemap no Astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://axion.com.br',
@@ -7,6 +8,7 @@ export default defineConfig({
     tailwind({
       applyBaseStyles: false,
     }),
+    sitemap(),
   ],
   compressHTML: true,
   build: {
