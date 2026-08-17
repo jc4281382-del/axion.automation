e2eacce13b15d3c98347aad99305c1bce9739e46
Jean Oliveira <jc4281382@gmail.com>
2026-08-17 15:13:14 -0300

    Corrige build do Astro removendo integracao duplicada de sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://axion.com.br',
@@ -8,7 +7,6 @@ export default defineConfig({
     tailwind({
       applyBaseStyles: false,
     }),
-    sitemap(),
   ],
   compressHTML: true,
   build: {
